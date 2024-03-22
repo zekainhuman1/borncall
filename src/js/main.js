adf8d92226bdafc0a62d5f4c98425b58de177aaf
zekainhuman1 <zekainhuman@gmail.com>
2024-03-22 14:22:26 +0200

    mobileSecond

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import '../scss/style.scss'
+
+document.addEventListener('DOMContentLoaded', function () {
+    var services = document.querySelectorAll('.service');
+    var windowHeight = window.innerHeight;
+
+    function checkScroll() {
+        services.forEach(function (service) {
+            var servicePosition = service.getBoundingClientRect().top;
+            if (servicePosition - windowHeight <= 0) {
+                service.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkScroll);
+    checkScroll();
+});
