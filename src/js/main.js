1005975f8389d7f31b5fc9670261930ef35d1281
zekainhuman1 <zekainhuman@gmail.com>
2024-03-27 14:27:48 +0200

    font

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import '../scss/style.scss'
 
+
 document.addEventListener('DOMContentLoaded', function () {
     var services = document.querySelectorAll('.service');
     var windowHeight = window.innerHeight;
@@ -26,3 +27,34 @@ input.addEventListener('change', function () {
         document.body.style.overflow = '';
     }
 });
+
+
+export function showForm() {
+    var modal = document.getElementById("modal");
+    var overlay = document.getElementById("overlay");
+    modal.style.display = "block";
+    overlay.style.display = "block";
+};
+
+export function closeModal() {
+    var modal = document.getElementById("modal");
+    var overlay = document.getElementById("overlay");
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var phoneField = document.getElementById('phoneField');
+
+    phoneField.addEventListener('input', function () {
+        if (!phoneField.validity.patternMismatch) {
+            phoneField.setCustomValidity('');
+        } else {
+            phoneField.setCustomValidity('Будь ласка, введіть номер телефону в форматі: +380 (XX) XXX-XX-XX');
+        }
+    });
+});
+
+
+window.showForm = showForm;
+window.closeModal = closeModal;
