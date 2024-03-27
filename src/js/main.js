df32f137d79f6a18409ea5a4417bd5c3663ee9a6
zekainhuman1 <zekainhuman@gmail.com>
2024-03-27 16:58:17 +0200

    desktop adapt

--- src/js/main.js
+++ src/js/main.js
@@ -50,11 +50,36 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!phoneField.validity.patternMismatch) {
             phoneField.setCustomValidity('');
         } else {
-            phoneField.setCustomValidity('Будь ласка, введіть номер телефону в форматі: +380 (XX) XXX-XX-XX');
+            phoneField.setCustomValidity('Будь ласка, введіть номер телефону в форматі: +380XXXXXXXXX, де Х це цифра');
         }
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('.modal__form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        closeModal();
+        showConfirmation();
+    });
+});
+
+function showConfirmation() {
+    var confirmationModal = document.getElementById('confirmationModal');
+    var overlay = document.getElementById('overlay');
+
+    confirmationModal.style.display = 'block';
+    overlay.style.display = 'block';
+
+    setTimeout(function () {
+        confirmationModal.style.display = 'none';
+        overlay.style.display = 'none';
+    }, 3000);
+}
+
 
 window.showForm = showForm;
 window.closeModal = closeModal;
+// window.showConfirmation = showConfirmation;
